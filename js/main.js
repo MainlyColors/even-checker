1455846083013717aaf919584481dddecad68836
Ryan <ryanmahara@gmail.com>
2022-04-15 15:13:18 -0500

    add iseven check, HTML/CSS to support function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function createResultElement(result) {
+  const h2 = document.createElement('h2');
+  h2.textContent = String(result).toUpperCase();
+  h2.classList.add('result');
+  h2.classList.add(String(result));
+
+  return h2;
+}
+
+const outputEl = document.querySelector('#output');
+function getFetch() {
+  const choice = document.querySelector('input').value;
+
+  if (choice.match(/[^0-9]+/g)?.length <= 0) {
+    document.querySelector('input').value = '';
+    alert(`can you not read? Numbers only 😂
+
+"${choice}" - does this look like a number to you???`);
+    return;
+  }
+
+  const url = `https://api.isevenapi.xyz/api/iseven/${choice}/`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      outputEl.innerHTML = '';
+
+      outputEl.appendChild(createResultElement(data.iseven));
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
